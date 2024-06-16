e6e82242dacac410f369c0d750bcd0bf5b192da5
bsylve1996 <bsylve1996@gmail.com>
2024-06-16 16:50:07 -0500

    test

--- composeApp/src/androidMain/kotlin/org/alexafit/fitjournal/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/alexafit/fitjournal/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
+        setContent {// test for things
             App()
         }
     }
